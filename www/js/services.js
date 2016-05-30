59d6d43c682c1395b1bcd778942e88850c0d8d61
rupert <rupert@2rmobile.com>
2016-05-30 21:46:32 +1000

    Sample angular HttpService based on appcamp.io network

--- www/js/services.js
+++ www/js/services.js
@@ -21,3 +21,28 @@ angular.module('apartmentsify.services', [])
     }
     return o;
   });
+
+angular.module('apartmentsify.services', [])
+.service('HttpService', function($http) {
+   return {
+     getPost: function() {
+       // $http returns a promise, which has a then function, which also returns a promise.
+       return $http.get('http://jsonplaceholder.typicode.com/posts/1')
+         .then(function (response) {
+           // In the response, resp.data contains the result. Check the console to see all of the data returned.
+           console.log('Get Post', response);
+           return response.data;
+         });
+     },
+
+     getUsers: function() {
+       // $http returns a promise, which has a then function, which also returns a promise.
+       return $http.get('http://jsonplaceholder.typicode.com/users')
+         .then(function(response) {
+           // In the response, resp.data contains the result. Check the console to see all of the data returned.
+           console.log('Get Users', response);
+           return response.data;
+        });
+     }
+   };
+  })
